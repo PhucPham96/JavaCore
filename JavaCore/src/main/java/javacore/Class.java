307ba74c42a8abf6011da956b4ad70885dc02112
ADMIN <ADMIN@DESKTOP-M971O36>
2021-10-18 15:50:04 +0700

    Add new class

---
+++ JavaCore/src/main/java/javacore/Class.java
@@ -0,0 +1,23 @@
+package javacore;
+
+public class Class {
+	private String classId;
+
+	public String getClassId() {
+		return classId;
+	}
+
+	public void setClassId(String classId) {
+		this.classId = classId;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	private String className;
+}
